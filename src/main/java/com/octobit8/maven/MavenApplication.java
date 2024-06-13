1c975c07b7ce2ff9d28e614f02c65929572b408e
zebafarah <zebafarah85@gmail.com>
2024-06-13 22:29:52 +0530

    Initial version

---
+++ src/main/java/com/octobit8/maven/MavenApplication.java
@@ -0,0 +1,13 @@
+package com.octobit8.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenApplication.class, args);
+	}
+
+}
